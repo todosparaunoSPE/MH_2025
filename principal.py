5437ed71225f97eb6c1431aafc6590252aa58d9e
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2025-03-12 12:34:24 -0600

    Update principal.py

--- principal.py
+++ principal.py
@@ -14,14 +14,14 @@ st.title("CAP de las AFORE y Nuevas Ubicaciones de CAP para PENSIONISSSTE")
 st.sidebar.title("Información")
 st.sidebar.write("Nombre: Javier Horacio Pérez Ricárdez")
 
-# Estilo CSS para la marca de agua
+# Estilo CSS para la marca de agua en la parte superior izquierda
 st.markdown(
     """
     <style>
     .watermark {
         position: fixed;
-        bottom: 10px;
-        right: 10px;
+        top: 10px;  # Cambia 'bottom' por 'top'
+        left: 10px;  # Cambia 'right' por 'left'
         opacity: 0.6;
         font-size: 18px;
         font-weight: bold;

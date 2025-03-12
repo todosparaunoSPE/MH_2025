4e63bde61ea0e37a2d8c26783123bd79d893cb86
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2025-03-12 12:42:48 -0600

    Update 1_Datos_originales.py

--- pages/1_Datos_originales.py
+++ pages/1_Datos_originales.py
@@ -94,18 +94,22 @@ st.plotly_chart(fig)
 
 
 
-# Estilo CSS para la marca de agua
+# Estilo CSS para la marca de agua en la parte inferior izquierda
 st.markdown(
     """
     <style>
     .watermark {
         position: fixed;
-        bottom: 10px;
-        right: 10px;
+        bottom: 10px;  # Coloca la marca de agua en la parte inferior
+        left: 10px;    # Alinea la marca de agua a la izquierda
         opacity: 0.6;
         font-size: 18px;
         font-weight: bold;
         color: gray;
+        z-index: 1000;  # Asegura que la marca de agua esté por encima de otros elementos
+        text-align: left;  # Alinea el texto a la izquierda
+        width: auto;  # Evita que el contenedor ocupe todo el ancho
+        white-space: nowrap;  # Evita que el texto se divida en varias líneas
     }
     </style>
     <div class="watermark">Javier Horacio Pérez Ricárdez</div>
